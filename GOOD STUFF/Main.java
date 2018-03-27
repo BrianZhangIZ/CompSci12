78919c07b3823d32d408907b723be1513258a835
nuttiercoder32 <37551561+nuttycoder23@users.noreply.github.com>
2018-03-27 15:17:14 -0400

    March 27th
    movement, Player, main
    -keyboard input is retrieved and turned into a direction for the character

---
+++ GOOD STUFF/Main.java
@@ -0,0 +1,20 @@
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Main
+{ 
+   Player player1 = new Player(50, 50);
+   Player player2 = new Player(100, 100);
+   
+   public void paint(Graphics g)
+   {
+      
+   }
+   public static void main(String[] args)
+   {
+      Pl
+   }
+}
---
+++ GOOD STUFF/Player.java
@@ -0,0 +1,46 @@
+class Player 
+{
+   int x;
+   int y;
+   movement direction;
+   
+   public Player(playerX, playerY)
+   {
+      x = playerX;
+      y = playerY;
+      direction = new movement();
+   }  
+   
+   
+   //public move(){}
+   
+   public static void main(String[] args){
+      new movement();
+   }
+   
+   public draw()
+   {
+      switch(direction.characterDirection())
+      {
+         case 1:
+            break;
+         case 2:
+            break;
+         case 3:
+            break;
+         case 4:
+            break;
+         case 5:
+            break;
+         case 6:
+            break;
+         case 7:
+            break;
+         case 8:
+            break;
+         default:
+            break;
+      }   
+   }
+}
+
